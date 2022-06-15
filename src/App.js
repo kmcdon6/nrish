64fa4c11e273adba44908b1aa080f8c05b258805
Kevin McDonough <kmcdon6@gmail.com>
2022-06-15 08:08:06 -0400

    create recipe cards

---
+++ src/App.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import { MDBBtn, MDBCol,MDBCard, MDBCardBody,MDBCardTitle,MDBCardText,MDBCardImage,MDBRipple,MDBRow} from 'mdb-react-ui-kit';
+// import Card from './Card';
+
+function App() {
+  return (
+      <MDBRow className='row-cols-1 row-cols-md-5 g-4'>
+<MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description1
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description2
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description3
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description4
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description5
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description6
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description7
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description8
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description9
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description10
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description11
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description12
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description13
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description14
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+        <MDBCard>
+        <MDBCardBody>
+          <MDBCardTitle>
+            Recipe Title
+          </MDBCardTitle>
+          <MDBCardText>
+            recipe description15
+          </MDBCardText>
+          <MDBBtn>
+            Button
+          </MDBBtn>
+          
+        </MDBCardBody>
+        </MDBCard>
+        </MDBCol>       
+
+        </MDBRow>
+
+
+  )
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MDBBtn, MDBContainer,MDBCard, MDBCardBody,MDBCardTitle,MDBCardText,MDBCardImage,MDBRipple} from 'mdb-react-ui-kit';
+
+
+// function Card(){
+//     return(
+//         <MDBCardTitle>
+//             title
+//             <MDBCardTitle/>
+        
+//     )
+// }
+// export default Card
